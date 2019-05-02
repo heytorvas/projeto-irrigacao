82d12a37d8d28b803641710c4a766d2ea2904bb2
Matheus Pimentel <matheuspcouto70@gmail.com>
2019-05-02 15:59:15 -0300

    Teste na página web sem o uso de gráficos

--- src/controller/IrrigacaoController.java
+++ src/controller/IrrigacaoController.java
@@ -2,12 +2,8 @@ package controller;
 
 import java.io.Serializable;
 
-import java.util.InputMismatchException;
-
 import javax.faces.view.ViewScoped;
 import javax.inject.Named;
-
-import application.Util;
 
 import java.io.BufferedReader;
 import java.io.FileReader;
@@ -17,11 +13,13 @@ import java.util.List;
 @Named
 @ViewScoped
 public class IrrigacaoController implements Serializable {
-
 	private static final long serialVersionUID = -8248257843709767818L;
 
 	private String dados;
 	String luminosidade;
+	String umidadeAr;
+	String temperaturaAr;
+	String statusSolo;
 
 	public String getDados() {
 		System.out.println("chamou getDados");
@@ -29,45 +27,74 @@ public class IrrigacaoController implements Serializable {
 	}
 
 	public void setDados(String dados) {
-		this.dados = dados;
 		try {
 			BufferedReader arquivo = new BufferedReader(new FileReader(
-					"C:\\Users\\mathe\\OneDrive\\Documentos\\UNITINS\\ESTR DADOS\\Historico_de_Log.txt"));
+					"C:\\Users\\mathe\\OneDrive\\Documentos\\UNITINS\\ESTR DADOS\\ProjetoIrrigacao\\Historico_de_Log.txt"));
 
 			List<String> arquivoArray = new ArrayList<String>();
 
 			while (arquivo.ready()) {
-
 				arquivoArray.add(arquivo.readLine());
-
 			}
 
 			arquivo.close();
 
 			int quantidadeElementosLista = arquivoArray.size();
-			System.out.println("LINHA: " + arquivoArray.size());
+			// System.out.println("TOTAL DE LINHAS = " + arquivoArray.size());
 
-			// int umidadeAr = quantidadeElementosLista - 5;
-			// int temperaturaAr = quantidadeElementosLista - 4;
-			// int leituraSolo = quantidadeElementosLista - 2;
+			int posicaoUmidadeAr = quantidadeElementosLista - 5;
+			umidadeAr = arquivoArray.get(posicaoUmidadeAr);
+
+			int posicaoTemperaturaAr = quantidadeElementosLista - 4;
+			temperaturaAr = arquivoArray.get(posicaoTemperaturaAr);
 
 			int posicaoLuminosidade = quantidadeElementosLista - 3;
 			luminosidade = arquivoArray.get(posicaoLuminosidade);
 
-			System.out.println("teste" + arquivoArray.get(posicaoLuminosidade));
+			int posicaoStatusSolo = quantidadeElementosLista - 2;
+			statusSolo = arquivoArray.get(posicaoStatusSolo);
+
+			this.dados = dados;
 
 		} catch (Exception e) {
+			System.out.println("falha na leitura");
 			e.printStackTrace();
 		}
-
 	}
 
 	public String getLuminosidade() {
 		String retorno = "";
 		if (dados != null) {
-			retorno = luminosidade;
+			String[] auxiliar = luminosidade.trim().split(":");
+			retorno = auxiliar[1] + "/1023";
 		}
 		return retorno;
 	}
-//    
+
+	public String getStatusSolo() {
+		String retorno = "";
+		if (dados != null) {
+			String[] auxiliar = statusSolo.trim().split(":");
+			retorno = auxiliar[1];
+		}
+		return retorno;
+	}
+
+	public String getUmidadeAr() {
+		String retorno = "";
+		if (dados != null) {
+			String[] auxiliar = umidadeAr.trim().split(":");
+			retorno = auxiliar[1];
+		}
+		return retorno;
+	}
+
+	public String getTemperaturaAr() {
+		String retorno = "";
+		if (dados != null) {
+			String[] auxiliar = temperaturaAr.trim().split(":");
+			retorno = auxiliar[1];
+		}
+		return retorno;
+	}
 }
